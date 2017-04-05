b332a60384c358d06e21ae15883c241218a35fcf
Cubiccl <ccl.clement@hotmail.fr>
2017-04-05 15:50:03 +0200

    Ajout calcul temps d'éxec

--- src/voyageur/Main.java
+++ src/voyageur/Main.java
@@ -10,6 +10,10 @@ public class Main
 
 	public static Window window;
 
+	/** Applique la méthode 2-opt pour le Circuit en entrée.
+	 * 
+	 * @param c - Le circuit à optimiser.
+	 * @return Un nouvel Objet Circuit */
 	public static Circuit apply2Opt(Circuit c)
 	{
 		boolean improved = true;
@@ -30,31 +34,57 @@ public class Main
 		return c;
 	}
 
-	public static void main(String[] args)
-	{
-		window = new Window();
-		window.setVisible(true);
-	}
-
-	public static void previousMain(int graphes, int sommets, boolean r, boolean g, boolean k, boolean m)
+	/** Applique différentes Méthodes de résolution du problème.
+	 * 
+	 * @param graphes - Le nombre de graphes à tester.
+	 * @param sommets - Le nombre de sommets par graphe.
+	 * @param r - Si vrai, la méthode Random est appliquée.
+	 * @param g - Si vrai, la méthode Glouton est appliquée.
+	 * @param k - Si vrai, la méthode de Kruskal est appliquée.
+	 * @param m - Si vrai, la méthode de Kruskal sera optimisée avec 2-opt.<br/>
+	 *            Toutes ces méthodes peuvent être appliquées ou non indépendemment. */
+	public static void calcul(int graphes, int sommets, boolean r, boolean g, boolean k, boolean m)
 	{
 		window.addText("Graphes: " + graphes + ", Sommets: " + sommets);
 
-		ArrayList<Double> aleatoire = new ArrayList<Double>(), glouton = new ArrayList<Double>(), kruskal = new ArrayList<Double>(), m2opt = new ArrayList<Double>();
+		ArrayList<Double> aleatoire = new ArrayList<Double>(), rTimes = new ArrayList<Double>();
+		ArrayList<Double> glouton = new ArrayList<Double>(), gTimes = new ArrayList<Double>();
+		ArrayList<Double> kruskal = new ArrayList<Double>(), kTimes = new ArrayList<Double>();
+		ArrayList<Double> m2opt = new ArrayList<Double>(), mTimes = new ArrayList<Double>();
+		double t = 0;
+
 		for (int i = 0; i < graphes; ++i)
 		{
 			if (graphes < 10) System.out.println("Calcul graphe " + i + "...");
 			TSP graphe = new TSP(sommets);
-			if (r) aleatoire.add(graphe.aleatoire().longueur());
-			if (g) glouton.add(graphe.glouton().longueur());
+			if (r)
+			{
+				t = System.currentTimeMillis();
+				aleatoire.add(graphe.aleatoire().longueur());
+				t = System.currentTimeMillis() - t;
+				rTimes.add(t);
+			}
+			if (g)
+			{
+				t = System.currentTimeMillis();
+				glouton.add(graphe.glouton().longueur());
+				t = System.currentTimeMillis() - t;
+				gTimes.add(t);
+			}
 			if (k)
 			{
+				t = System.currentTimeMillis();
 				Circuit c = graphe.kruskal();
 				kruskal.add(c.longueur());
+				t = System.currentTimeMillis() - t;
+				kTimes.add(t);
 				if (m)
 				{
+					t = System.currentTimeMillis();
 					c = apply2Opt(c);
 					m2opt.add(c.longueur());
+					t = System.currentTimeMillis() - t;
+					mTimes.add(t);
 				}
 			}
 		}
@@ -67,6 +97,11 @@ public class Main
 				moy += d;
 			moy /= aleatoire.size();
 			window.addText("Distance moyenne pour aléatoire: " + moy);
+			moy = 0;
+			for (Double d : rTimes)
+				moy += d;
+			moy /= rTimes.size();
+			window.addText("Temps moyen pour aléatoire: " + moy);
 		}
 
 		if (g)
@@ -76,6 +111,10 @@ public class Main
 				moy += d;
 			moy /= glouton.size();
 			window.addText("Distance moyenne pour Glouton: " + moy);
+			for (Double d : gTimes)
+				moy += d;
+			moy /= gTimes.size();
+			window.addText("Temps moyen pour Glouton: " + moy);
 		}
 
 		if (k)
@@ -85,6 +124,10 @@ public class Main
 				moy += d;
 			moy /= kruskal.size();
 			window.addText("Distance moyenne pour Kruskal: " + moy);
+			for (Double d : kTimes)
+				moy += d;
+			moy /= kTimes.size();
+			window.addText("Temps moyen pour Kruskal: " + moy);
 
 			if (m)
 			{
@@ -92,8 +135,18 @@ public class Main
 				for (Double d : m2opt)
 					moy += d;
 				moy /= m2opt.size();
-				window.addText("Distance moyenne pour Kruskal après 2-opt: " + moy);
+				window.addText("Distance moyenne pour 2-opt: " + moy);
+				for (Double d : mTimes)
+					moy += d;
+				moy /= mTimes.size();
+				window.addText("Temps moyen pour 2-opt: " + moy);
 			}
 		}
+	}
+
+	public static void main(String[] args)
+	{
+		window = new Window();
+		window.setVisible(true);
 	}
 }
